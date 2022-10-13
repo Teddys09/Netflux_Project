3ffe2a2c0282c9d226a1a371e0ea4988cee1742c
Teddys09 <ted3009@hotmail.fr>
2022-10-13 16:03:20 +0200

    Add content when my list Have no Media

--- src/Pages/MyList.js
+++ src/Pages/MyList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import CallUseFetch from '../utils/CallUseFetch';
 import Footer from '../components/Footer';
 import Header from '../components/Header/Header';
 import { useSelector } from 'react-redux';
@@ -7,6 +7,7 @@ import { AiFillCaretRight } from 'react-icons/ai';
 import { AiFillCaretLeft } from 'react-icons/ai';
 
 const MyList = () => {
+  CallUseFetch();
   const userList = useSelector((state) => state.media.userList);
 
   const baseUrl = 'http://image.tmdb.org/t/p/original';
@@ -36,44 +37,58 @@ const MyList = () => {
       caretClickedRight ? `${translateRight}%` : `${translateRight}%`
     })`,
   };
-
-  return (
-    <div className="my-list">
-      <Header />
-      <div className="home-container">
-        <div className="card-container ">
-          <h2 className="card-title-list">Your List</h2>
-          <div className="card-images">
-            <div className="caret-left">
-              <AiFillCaretLeft onClick={() => handleCaretClickLeft()} />
-            </div>
-            {userList.map((item) => (
-              <div
-                className="image-container"
-                key={item.id}
-                style={containerImageStyle}
-              >
-                <div
-                  style={{
-                    backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.1)),url(${
-                      baseUrl + item.poster_path
-                    })`,
-                  }}
-                  alt={item.title || item.name}
-                  id={item.title || item.name}
-                  className="card-list"
-                ></div>
+  if (userList.length === 0) {
+    return (
+      <div className="my-list">
+        <Header />
+        <div className="my-list-container">
+          <h2 className="my-list-title">My List</h2>
+          <div className="my-list-content">
+            <p className="my-list-text">You have no items in your list</p>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div className="my-list">
+        <Header />
+        <div className="home-container">
+          <div className="card-container ">
+            <h2 className="card-title-list">Your List</h2>
+            <div className="card-images">
+              <div className="caret-left">
+                <AiFillCaretLeft onClick={() => handleCaretClickLeft()} />
               </div>
-            ))}
-            <div className="caret-right">
-              <AiFillCaretRight onClick={() => handleCaretClickRight()} />
+              {userList.map((item) => (
+                <div
+                  className="image-container"
+                  key={item.id}
+                  style={containerImageStyle}
+                >
+                  <div
+                    style={{
+                      backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.1)),url(${
+                        baseUrl + item.poster_path
+                      })`,
+                    }}
+                    alt={item.title || item.name}
+                    id={item.title || item.name}
+                    className="card-list"
+                  ></div>
+                </div>
+              ))}
+              <div className="caret-right">
+                <AiFillCaretRight onClick={() => handleCaretClickRight()} />
+              </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 };
 
 export default MyList;
